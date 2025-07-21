9b4c18cfc3ecc235724bda1389b9995370829d3a
war_goddess300 <154751143+tashaglam971@users.noreply.github.com>
2025-07-21 00:47:16 -0400

    variableSeenInTextFile.C

---
+++ variableSeenInTextFile.C
@@ -0,0 +1,33 @@
+/*war goddess 300 backdoor test
+ *
+ * i'm a beginner, i try to see if what is entered in the input
+ * will be stored in the file (.txt)
+ * i'm an ultra beginner learning C, C++, rust, SQL, networks,
+ * linux, OpenBSD, etc
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+char variable[20];
+
+FILE *fptr;
+fptr = fopen("test.txt", "w");
+
+printf("Enter your first name: ");
+fgets(variable, sizeof(variable), stdin);
+variable[strcspn(variable, "\n")] = 0;
+
+fprintf(fptr, "%s", variable);
+
+printf("%s, what you entered stored in the variable \"variable\" at the memory address %p is stored in a file", variable, &variable);
+
+
+
+	return 0;
+
+}
