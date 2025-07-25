5de00b5a22de4ed54d2bb73d49aa3eb93e8d9448
⚡️🌩️war goddess 300⚡️ <154751143+warGoddess300@users.noreply.github.com>
2025-07-25 09:02:07 -0400

    Update variableSeenInTextFile.C

--- variableSeenInTextFile.C
+++ variableSeenInTextFile.C
@@ -1,7 +1,6 @@
-/*war goddess 300 
-
+/*war goddess 300 backdoor test
  *
- * i'm an ultra beginner, i try to see if what is entered in the input
+ * i'm a beginner, i try to see if what is entered in the input
  * will be stored in the file (.txt)
  * i'm an ultra beginner learning C, C++, rust, SQL, networks,
  * linux, OpenBSD, etc
@@ -16,14 +15,17 @@ int main() {
 
 char variable[20];
 
-FILE *fptr;
-fptr = fopen("test.txt", "w");
+FILE *input_to_text_file;
+input_to_text_file = fopen("test.txt", "w");
+
+// Statements must end in a semicolon (;)
+// Code elements are case sensitive
 
 printf("Enter your first name: ");
 fgets(variable, sizeof(variable), stdin);
 variable[strcspn(variable, "\n")] = 0;
 
-fprintf(fptr, "%s", variable);
+fprintf(input_to_text_file, "%s", variable);
 
 printf("%s, what you entered stored in the variable \"variable\" at the memory address %p is stored in a file", variable, &variable);
 
